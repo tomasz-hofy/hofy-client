877048740a59eff6ed124feb019b3dcb34f1db7e
Tomasz Bialecki <tomasz.bialecki@siili.com>
2021-01-05 09:58:04 +0100

    cleanup

--- src/components/icons/SvgIcon.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.SvgIcon = void 0;
-var account_svg_1 = require("../../assets/icons/account.svg");
-exports.SvgIcon = {
-    Account: account_svg_1["default"]
-};
